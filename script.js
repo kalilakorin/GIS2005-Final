0aa6200158fa45d34a1190dd5ba2d671f5ea1945
kalilakorin <kalila.korin@gmail.com>
2024-12-08 20:02:27 -0700

    Update index to utilize experience builder only

--- script.js
+++
@@ -1,7 +0,0 @@
-function navigateTo(page) {
-    window.location.href = page;
-
-    // Highlight the active button
-    document.querySelectorAll('button').forEach(btn => btn.classList.remove('active'));
-    document.getElementById(buttonId).classList.add('active');
-}
